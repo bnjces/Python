7dc5aca724c16e534252a8900b07894fede870b5
bnjces <bnjces@gmail.com>
2023-02-27 23:39:05 -0500

    Tiny addition.

--- cats.py
+++ cats.py
@@ -3,6 +3,8 @@ numCats = input()
 try:
     if int(numCats) >= 4:
         print('That is a lot of cats.')
+    elif int(numCats) <= 0:
+        print('Um, it is not possible to have a negative amount of cats.')
     else:
         print('That is not that many cats.')
 except ValueError:
